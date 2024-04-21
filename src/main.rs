2e2290bb57fa5d9773349f6c6566ff057839b8a8
semar <semaraugusto@gmail.com>
2024-04-21 10:49:33 -0300

    can run matmul in candle tensors

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,23 @@
 #![no_main]
 sp1_zkvm::entrypoint!(main);
 
-use serde::{Deserialize, Serialize};
-use std::hint::black_box;
+// use serde::{Deserialize, Serialize};
+// use std::hint::black_box;
+use candle_core::{Device, Tensor};
 
-#[derive(Serialize, Deserialize, Debug, PartialEq)]
-struct MyPointUnaligned {
-    pub x: f64,
-    pub y: f64,
-    pub b: bool,
-}
+// fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() {
+    let device = Device::Cpu;
 
-pub fn main() {
-    // let p1 = sp1_zkvm::io::read::<MyPointUnaligned>();
-    // println!("Read point: {:?}", p1);
-    //
-    // let p2 = sp1_zkvm::io::read::<MyPointUnaligned>();
-    // println!("Read point: {:?}", p2);
-    let p1 = black_box(MyPointUnaligned {
-        x: black_box(1.5),
-        y: black_box(2.5),
-        b: black_box(true),
-    });
+    // let a = Tensor::new(&[1., 2., 3., 4., 5.], &device).unwrap();
+    // let b = Tensor::new(&[1., 2., 3., 4., 5.], &device).unwrap();
+    let a = Tensor::new(&[[1., 2., 3.], [1., 2., 3.], [1., 2., 3.]], &device).unwrap();
+    let b = Tensor::new(&[[1., 1., 1.], [2., 2., 2.], [3., 3., 3.]], &device).unwrap();
 
-    let p2 = black_box(MyPointUnaligned {
-        x: black_box(0.5),
-        y: black_box(0.5),
-        b: black_box(false),
-    });
+    let c = a.matmul(&b).unwrap();
+    // let c = a.mul(&b).unwrap();
 
-    let p3: MyPointUnaligned = MyPointUnaligned {
-        x: p1.x + p2.x,
-        y: p1.y + p2.y,
-        b: p1.b && p2.b,
-    };
-    println!("1 Addition of 2 points: {:?}", p3);
-    // sp1_zkvm::io::commit(&p3);
-    let p4: MyPointUnaligned = MyPointUnaligned {
-        x: p1.x + p3.x,
-        y: p1.y + p3.y,
-        b: p1.b && p3.b,
-    };
-
-    println!("2 Addition of 2 points: {:?}", p4);
-    println!("3 END!");
+    println!("{c:?}");
+    println!("`{c}`");
+    // Ok(())
 }
